257f98e2734f3094613a3c58171b0bc6b6835ac7
Josh Johnson <caellwyn@gmail.com>
2021-03-10 15:14:20 -0500

    further shrinking model to save RAM

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-from writing_predictor import predict_grade, load_model
+from writing_predictor import predict_grade
+from keras.models import load_model
+import en_core_web_sm
 
 # set initial app settings
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -10,7 +12,9 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 #Load the Model
-model = load_model()
+model = load_model('model-Bi-LSTM-best')
+spacy = en_core_web_sm.load()
+process = 'Grammar'
 
 # display layout and components
 app.layout = html.Div([
@@ -23,22 +27,7 @@ app.layout = html.Div([
         style={'width': '%100', 'height': 300},
     ),
     html.Div(id='sample-text-output', style={'whiteSpace': 'pre-line'})
-    # html.H4('Country'),
-    # html.Div(id='country-value'),
-    # dcc.Dropdown(
-    #     id='dropdown-country',
-    #     options=[{'label': i, 'value': i} for i in unique_countries],
-    #     value='None'
-    # ),
-    # html.H4('State'),
-    # html.Div(id='state-value'),
-    # dcc.Dropdown(id='dropdown-state',
-    #              options=[{'label': 'None', 'value': 'None'}],
-    #              value='None'
-    #              ),
-    # html.H4('Graph'),
-    # html.Div(id='graph')
-])
+
 
 
 @app.callback(
@@ -47,38 +36,10 @@ app.layout = html.Div([
 )
 def update_output(text):
     if len(text) > 1:
-        return predict_grade(model, text)
+        return predict_grade(model, text, process, spacy)
     else:
         return 0
 
-# set state options according to chosen country
-# @app.callback([dash.dependencies.Output('dropdown-state', 'options'),
-#                dash.dependencies.Output('dropdown-state', 'value')],
-#               [dash.dependencies.Input('dropdown-country', 'value')])
-# def add_states(country_value, df=full_df):
-#     country_df = full_df.loc[full_df['CountryName'] == country_value]
-#     state_df = country_df.loc[country_df['Jurisdiction']
-#                               == 'STATE_TOTAL'].dropna()
-#     global country
-#     country = country_value
-#     if len(state_df) > 0:
-#         return [{'label': 'None', 'value': 'None'}] + [{'label': i, 'value': i}
-#                                                        for i in sorted(state_df['RegionName'].dropna().unique())], 'None'
-#     else:
-#         return [{'label': 'None', 'value': 'None'}], 'None'
-
-
-# create graph
-# @app.callback(dash.dependencies.Output('graph', 'children'),
-#               [dash.dependencies.Input('dropdown-state', 'value'),
-#                dash.dependencies.Input('dropdown-country', 'value'),
-#                dash.dependencies.Input('checklist-stat', 'value')])
-# def display_value(state, country, stats, df=full_df):
-#     if country != 'None':
-#         return dcc.Graph(id='stat-graph',
-#                          figure=graph_stat(df, state=state,
-#                                            country=country,
-#                                            stats=stats))
 
 
 if __name__ == '__main__':
--- writing_predictor.py
+++ writing_predictor.py
@@ -1,28 +1,65 @@
 import pandas as pd
+import numpy as np
 import re
-import keras
-
 import warnings
 warnings.filterwarnings('ignore')
 
-def separate_sentences(df):
-    if type(df) == str:
-        df = re.split('\;|\n|\.|\?|\!', df)
-        df = pd.DataFrame(df, columns=['Text'])
-        df = df[df['Text'] != ''].dropna().reset_index(drop=True)
-    else:
-        df.Text = df.Text.str.split(pat='\;|\n|\.|\?|\!', expand=False)
-        df = df.explode('Text').dropna()
-        df = df[df.Text != ''].reset_index(drop=True)
+
+import re
+import pandas as pd
+
+def load_text():
+    import pandas as pd
+    df = pd.read_csv('samples_no_title.csv',
+                       skipinitialspace=True,
+                       sep=',', 
+                       quotechar='"', 
+                       escapechar='\\',
+                       error_bad_lines=False,
+                       usecols = ['Grade','Text']).dropna()
     return df
 
-def lower_case(string):
-    return string.lower()
+def separate_sentences(df_in):
+    if type(df_in) == str:
+        df = df_in
+        df = re.split('\;|\n|\.|\?|\!', df)
+        df = pd.DataFrame(df, columns=['Text'])
+        df = df[df.Text.str.len() > 4].dropna().reset_index(drop=True)
+    else:
+        df = df_in.copy()
+        df.Text = df.Text.str.split(pat='\;|\n|\.|\?|\!', expand=False)
+        df = df.explode('Text').dropna()
+        df = df[df.Text.str.len() > 4].reset_index(drop=True)
+    return df
 
-def load_model():
-    return keras.models.load_model('model-Bi-LSTM-best')
+def prepare_text(df, spacy_model, process='Lemmas'):
+    df.Text = df.Text.str.replace('\n',' ')
+    docs = df.Text.apply(spacy_model)
+    newtext = []
+    if process == 'Grammar':
+        for doc in docs:
+            grammar_text = ' '.join([f'{word.text} {word.pos_} {word.dep_}' for word in doc])
+            newtext.append(grammar_text)
+    elif process == 'Lemmas':
+        for doc in docs:
+            lemma_text = ' '.join([sent.lemma_ for sent in doc.sents])
+            newtext.append(lemma_text)
+    else:
+        print('Choose "Lemmas" or "Grammar"')
+        return None
+    
+    df[process] = newtext
+    return df
 
-def predict_grade(model, text):
-    text = separate_sentences(text)
-    text.Text = text.Text.apply(lower_case)
-    return model.predict(text.Text).mean()
+
+def predict_grade(model, corpus, process, spacy_model):
+    if type(corpus) == str:
+        corpus = separate_sentences(corpus)
+        corpus = prepare_text(corpus, spacy_model, process)
+        yhat = model.predict(corpus[process]).mean()
+        return yhat
+    else:
+        predictions = []
+        for text in corpus:
+            predictions.append(predict_grade(model, text, process, spacy_model))
+        return pd.Series(predictions)
